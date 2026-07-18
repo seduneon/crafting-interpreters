ee446ec1145757f1da890129c0f6ac5989df47b4
zhambylmaksotov <junkedseduneon@gmail.com>
2026-07-18 18:04:08 +0100

    jlox: Chapter 11 WIP

--- jlox/com/craftinginterpreters/lox/Interpreter.java
+++ jlox/com/craftinginterpreters/lox/Interpreter.java
@@ -51,6 +51,20 @@ class Interpreter implements Expr.Visitor<Object>,
     return null;
   }
 
+  @Override
+  public Void visitClassStmt(Stmt.Class stmt) {
+    environment.define(stmt.name.lexeme, null);
+    Map<String, LoxFunction> methods = new HashMap<>();
+    for (Stmt.Function method : stmt.methods) {
+      LoxFunction function = new LoxFunction(method, environment,
+                      method.name.lexeme.equals("init"));
+      methods.put(method.name.lexeme, function);
+    }
+    LoxClass klass = new LoxClass(stmt.name.lexeme, methods);
+    environment.assign(stmt.name, klass);
+    return null;
+  }
+
   void executeBlock(List<Stmt> statements,
                     Environment environment) {
     Environment previous = this.environment;
@@ -90,6 +104,23 @@ class Interpreter implements Expr.Visitor<Object>,
       if (!isTruthy(left)) return left;
     }
     return evaluate(expr.right);
+  }
+
+  @Override
+  public Object visitSetExpr(Expr.Set expr) {
+    Object object = evaluate(expr.object);
+    if (!(object instanceof LoxInstance)) {
+      throw new RuntimeError(expr.name,
+                        "Only instances have fields.");
+    }
+    Object value = evaluate(expr.value);
+    ((LoxInstance)object).set(expr.name, value);
+    return value;
+  }
+
+  @Override
+  public Object visitThisExpr(Expr.This expr) {
+    return lookUpVariable(expr.keyword, expr);
   }
 
   @Override
@@ -182,6 +213,16 @@ class Interpreter implements Expr.Visitor<Object>,
     return function.call(this, arguments);
   }
 
+  @Override
+  public Object visitGetExpr(Expr.Get expr) {
+    Object object = evaluate(expr.object);
+    if (object instanceof LoxInstance) {
+      return ((LoxInstance) object).get(expr.name);
+    }
+    throw new RuntimeError(expr.name,
+          "Only instances have properties.");
+  }
+
   private boolean isEqual(Object a, Object b) {
     if (a == null && b == null) return true;
     if (a == null) return false;
@@ -207,7 +248,8 @@ class Interpreter implements Expr.Visitor<Object>,
 
   @Override
   public Void visitFunctionStmt(Stmt.Function stmt) {
-    LoxFunction function = new LoxFunction(stmt, environment);
+    LoxFunction function = new LoxFunction(stmt, environment,
+                                                false);
     environment.define(stmt.name.lexeme, function);
     return null;
   }
--- jlox/com/craftinginterpreters/lox/Resolver.java
+++ jlox/com/craftinginterpreters/lox/Resolver.java
@@ -6,10 +6,23 @@ import java.util.Map;
 import java.util.Stack;
 
 class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
+  private enum ClassType {
+    NONE,
+    CLASS
+  }
+
+  private enum FunctionType {
+    NONE,
+    FUNCTION,
+    METHOD,
+    INITIALIZER
+  }
+
   private final Interpreter interpreter;
   private final Stack<Map<String, Boolean>> scopes = new Stack<>();
-  private FunctionType currentFunction FunctionType.NONE;
-
+  private FunctionType currentFunction = FunctionType.NONE;
+  private ClassType currentClass = ClassType.NONE;
+   
   Resolver(Interpreter interpreter) {
     this.interpreter = interpreter;
   }
@@ -19,6 +32,26 @@ class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
     beginScope();
     resolve(stmt.statements);
     endScope();
+    return null;
+  }
+
+  @Override
+  public Void visitClassStmt(Stmt.Class stmt) {
+    ClassType enclosingClass = currentClass;
+    currentClass = ClassType.CLASS;
+    declare(stmt.name);
+    define(stmt.name);
+    beginScope();
+    scopes.peek().put("this", true);
+    for (Stmt.Function method : stmt.methods) {
+      FunctionType declaration = FunctionType.METHOD;
+      if (method.name.lexeme.equals("init")) {
+        declaration = FunctionType.INITIALIZER;
+      }
+      resolveFunction(method, declaration);
+    }
+    endScope();
+    currentClass = enclosingClass;
     return null;
   }
 
@@ -144,6 +177,10 @@ class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
       Lox.error(stmt.keyword, "Can't return from top-level code.");
     }
     if (stmt.value != null) {
+      if (currentFunction == FunctionType.INITIALIZER) {
+        Lox.error(stmt.keyword,
+              "Can't return a value from an initializer.");
+      }
       resolve(stmt.value);
     }
     return null;
@@ -173,6 +210,12 @@ class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
   }
 
   @Override
+  public Void visitGetExpr(Expr.Get expr) {
+    resolve(expr.object);
+    return null;
+  }
+
+  @Override
   public Void visitGroupingExpr(Expr.Grouping expr) {
     resolve(expr.expression);
     return null;
@@ -191,13 +234,27 @@ class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
   }
 
   @Override
+  public Void visitSetExpr(Expr.Set expr) {
+    resolve(expr.value);
+    resolve(expr.object);
+    return null;
+  }
+
+  @Override
+  public Void visitThisExpr(Expr.This expr) {
+    if (currentClass == ClassType.NONE) {
+      Lox.error(expr.keyword,
+          "Can't use 'this' outside of a class.");
+      return null;
+    }
+    resolveLocal(expr, expr.keyword);
+    return null;
+  }
+
+  @Override
   public Void visitUnaryExpr(Expr.Unary expr) {
     resolve(expr.right);
     return null;
   }
 }
 
-private enum FunctionType {
-  NONE,
-  FUNCTION
-}
