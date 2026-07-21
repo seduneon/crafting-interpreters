fdf71573f55b3cb3164e7353361c69e4ad08d13a
zhambylmaksotov <junkedseduneon@gmail.com>
2026-07-21 22:30:27 +0100

    clox: Chapter 20

--- clox/vm.h
+++ clox/vm.h
@@ -2,6 +2,7 @@
 #define clox_vm_h
 
 #include "chunk.h"
+#include "table.h"
 #include "value.h"
 
 #define STACK_MX 256
@@ -11,6 +12,7 @@ typedef struct {
   uint8_t* ip;
   Value stack[STACK_MX];
   Value *stackTop;
+  Table strings;
   Obj *objects;
 } VM;
   
